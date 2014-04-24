682783dc501f00903fdd21bedb26108544bf3f57
Ed Cranford <ed.cranford@rackspace.com>
2014-04-24 14:00:55 -0500

    add start-demo.sh and update paulmo's demo

---
+++ start-demo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /opt/stack/solum-gui/bridge/m1demo
+screen -dmS bridge
+screen -S bridge -p 0 -X stuff 'python ./manage.py runserver 0.0.0.0:9001
+'
+
+cd /opt/stack/solum-gui/ui/SolumDemoUI
+screen -dmS ui
+screen -S ui -p 0 -X stuff 'python -mSimpleHTTPServer 8001
+'
