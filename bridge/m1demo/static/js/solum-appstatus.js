3d0eadc6cf03a677771eee949b1e142eb7488d9d
Ed Cranford <ed.cranford@rackspace.com>
2014-04-30 15:30:46 -0500

    Updating app status js and adding some more icons

--- bridge/m1demo/static/js/solum-appstatus.js
+++ bridge/m1demo/static/js/solum-appstatus.js
@@ -1,176 +1,190 @@
-function draw_status(context, name, current, xoffset){
-  var FAILED = "#900";
-  var PENDING = "#039";
-  var PASSED = "#090";
-  var BLACK = "#000";
-  var DISABLED = "#ccc";
+function solum_draw_status(context, states){
+  var COLORS = {
+      FAILED: "#900",
+      PENDING: "#039",
+      PASSED: "#090",
+      DISABLED: "#ccc",
+      DEFAULT: "#000",
+    }
+  context.clearRect(0, 0, 500, 500);
 
   context.fillStyle = "transparent";
-  context.strokeStyle = BLACK;
-  context.lineWidth = "2";
+  context.strokeStyle = COLORS.DEFAULT;
   context.font = "16px Helvetica";
 
-  switch(current){
-    case 'ok':
-      context.fillStyle = PASSED;
-      context.strokeStyle = PASSED;
-    break;
-    case 'pending':
-      context.fillStyle = PENDING;
-      context.strokeStyle = PENDING;
-    break;
-    case 'error':
-      context.fillStyle = FAILED;
-      context.strokeStyle = FAILED;
-    break;
-    case 'disabled':
-      context.fillStyle = DISABLED;
-      context.strokeStyle = DISABLED;
-    break;
-    default:
-      context.fillStyle = BLACK;
-      context.strokeStyle = BLACK;
+  var icons = [];
+  for(var i=0; i<states.length; i++){
+    icons.push(new Image());
   }
-  context.lineWidth = "2";
-  context.strokeRect(10 + xoffset, 20, 60, 60);
-  context.lineWidth = "0";
-  context.fillText(name, 10 + xoffset, 95);
-}
 
-function update_status(context, stat) {
-  console.log("Updating status to " + stat + ".")
-  context.clearRect(0, 0, 500, 100);
+  for(var i=0; i<states.length; i++){
+    var state = states[i];
+    var color = COLORS.DEFAULT;
+    switch(state.status){
+      case 'PASSED':
+        color = COLORS.PASSED;
+        break;
+      case 'FAILED':
+        color = COLORS.FAILED;
+        break;
+      case 'PENDING':
+        color = COLORS.PENDING;
+        break;
+      case 'DISABLED':
+        color = COLORS.DISABLED;
+        break;
+      default:
+        color = COLORS.DEFAULT;
+    }
+    context.fillStyle = color;
+    context.strokeStyle = color;
 
-  draw_status(context, 'git', stat.git, 0);
-  draw_status(context, 'build', stat.build, 100);
-  draw_status(context, 'test', stat.test, 200);
-  draw_status(context, 'image', stat.image, 300);
-  draw_status(context, 'template', stat.template, 400);
+    context.lineWidth = "4";
+    var xoffset = 10 + (i * 100);
+    context.strokeRect(xoffset, 20, 60, 60);
+    context.lineWidth = "0";
+    context.fillText(state.name, xoffset, 95);
 
-  //draw four arrows between the states
-  context.fillStyle = "#000";
-  context.strokeStyle = "#000";
-  for(var i=80; i<=400; i+=100){
+    var icon = icons[i];
+    icon.src = '../images/' + state.img + '.png';
+    icon.xoff = 10 + (100 * i);
+    icon.onload = function(k) {
+        return function(){
+          context.drawImage(k, k.xoff, 20, 60, 60);
+        }
+    }(icon);
+  }
+
+  context.fillStyle = COLORS.DEFAULT;
+  context.strokeStyle = COLORS.DEFAULT;
+  for(var i=0; i<states.length-1; i++){
+    var x = 80 + (100 * i);
     context.beginPath();
-    context.arc(i,      50, 3, 0, 2*Math.PI);
-    context.arc(i + 10, 50, 3, 0, 2*Math.PI);
-    //context.arc(i + 20, 50, 3, 0, 2*Math.PI);
+    context.arc(x,      50, 3, 0, 2*Math.PI);
+    context.arc(x + 10, 50, 3, 0, 2*Math.PI);
     context.fill();
     context.beginPath()
-    context.moveTo(i + 17, 45);
-    context.lineTo(i + 27, 50);
-    context.lineTo(i + 17, 55);
+    context.moveTo(x + 17, 45);
+    context.lineTo(x + 27, 50);
+    context.lineTo(x + 17, 55);
     context.closePath();
     context.fill();
   }
 
+
 }
 
-function build_status(stat){
-  console.log("Stat is " + stat);
-  var all = {};
+function build_status(string){
+    var S = [
+        {
+            'name': 'git',
+            'img': 'download-icon',
+        },
+        {
+            'name': 'build',
+            'img': 'wrench',
+        },
+        {
+            'name': 'test',
+            'img': 'lab-beaker-icon',
+            'status': 'DISABLED',
+        },
+        {
+            'name': 'image',
+            'img': 'paper-icon',
+        },
+        {
+            'name': 'template',
+            'img': 'setting-icon',
+        },
+    ];
+    switch(string){
+        case 'PENDING':
+            break;
+        case 'BUILDING':
+            S[0].status = 'PASSED';
+            S[1].status = 'PENDING';
+            break;
+        case 'DEPLOYING':
+            S[0].status = 'PASSED';
+            S[1].status = 'PASSED';
+            S[3].status = 'PASSED';
+            S[4].status = 'PASSED';
+            break;
+        case 'READY':
+            S[0].status = 'PASSED';
+            S[1].status = 'PASSED';
+            S[3].status = 'PASSED';
+            S[4].status = 'PASSED';
+            break;
+        case 'ERROR':
+            S[0].status = 'PASSED';
+            S[1].status = 'PASSED';
+            S[3].status = 'PASSED';
+            S[4].status = 'PASSED';
+            break;
+        case 'ERROR_STACK_CREATE_FAILED':
+            S[0].status = 'PASSED';
+            S[1].status = 'PASSED';
+            S[3].status = 'PASSED';
+            S[4].status = 'PASSED';
+            break;
+    }
 
-  switch(stat){
-    case 'git':
-      all.git = 'pending';
-      break;
-    case 'git_error':
-      all.git = 'error';
-      break;
-
-    case 'build':
-      all.git = 'ok';
-      all.build = 'pending';
-      break;
-    case 'build_error':
-      all.git = 'ok';
-      all.build = 'error';
-      break;
-
-    case 'test':
-      all.git = 'ok';
-      all.build = 'ok';
-      all.test = 'pending';
-      break;
-    case 'test_error':
-      all.git = 'ok';
-      all.build = 'ok';
-      all.test = 'error';
-      break;
-
-    case 'image':
-      all.git = 'ok';
-      all.build = 'ok';
-      all.test = 'ok';
-      all.image = 'pending';
-      break;
-    case 'image_error':
-      all.git = 'ok';
-      all.build = 'ok';
-      all.test = 'ok';
-      all.image = 'error';
-      break;
-
-    case 'template':
-      all.git = 'ok';
-      all.build = 'ok';
-      all.test = 'ok';
-      all.image = 'ok';
-      all.template = 'pending';
-      break;
-    case 'template_error':
-      all.git = 'ok';
-      all.build = 'ok';
-      all.test = 'ok';
-      all.image = 'ok';
-      all.template = 'error';
-      break;
-
-    case 'done':
-      all.git = 'ok';
-      all.build = 'ok';
-      all.test = 'ok';
-      all.image = 'ok';
-      all.template = 'ok';
-      break;
-
-    default:
-      all.git = '';
-      all.build = '';
-      all.test = '';
-      all.image = '';
-      all.template = '';
-      break;
-  }
-  console.log(all);
-  return all;
+    return S;  
 }
 
-function popStatus() {
-  var stat = $("#appstatus").val();
-  stat = build_status(stat);
-  var canv = document.getElementById("appstatus");
-  if (canv.getContext){
-    var context = canv.getContext("2d");
-    update_status(context, stat);
-  }
+function deploy_status(string){
+    var S = [
+        {
+            'name': 'start',
+            'img': 'download-icon',
+        },
+        {
+            'name': 'heat',
+            'img': 'fire-icon',
+        },
+        {
+            'name': 'complete',
+            'img': 'browser-icon',
+        },
+    ];
+    switch(string){
+        case 'DEPLOYING':
+            S[0].status = 'PASSED';
+            S[1].status = 'PENDING';
+            break;
+        case 'READY':
+            S[0].status = 'PASSED';
+            S[1].status = 'PASSED';
+            S[2].status = 'PASSED';
+            break;
+        case 'ERROR':
+            S[0].status = 'PASSED';
+            S[1].status = 'FAILED';
+            break;
+        case 'ERROR_STACK_CREATE_FAILED':
+            S[0].status = 'FAILED';
+            break;
+    }
+
+    return S;  
 }
 
-function initialStatus(astatus) {
-  console.log("Setting initial status.")
-  var stat = build_status(astatus);
-  stat.test = 'disabled';
-  var canv = document.getElementById("appstatus");
+
+
+function update_status(status_string) {
+  var stat = build_status(status_string);
+  var canv = document.getElementById("buildstatus");
   if (canv.getContext){
     var context = canv.getContext("2d");
-    update_status(context, stat);
+    solum_draw_status(context, stat);
   }
 
-  stat = build_status("");
-  var canv = document.getElementById("depstatus");
+  stat = deploy_status(status_string);
+  var canv = document.getElementById("deploystatus");
   if (canv.getContext){
     var context = canv.getContext("2d");
-    update_status(context, stat);
+    solum_draw_status(context, stat);
   }
-
 }
