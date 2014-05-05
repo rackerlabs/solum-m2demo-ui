5c951c3aa0abddb5059019035e2aacc0b28da5c3
Paul Montgomery <PaulMontgomery.code@gmail.com>
2014-05-05 09:52:40 -0500

    The HTML5 canvas now pushes the plan file to the planfile text box after every icon connect/disconnect so that the data is always accurate.  Solum can not consume this advanced plan file so I will create another pull request to place a functioning plan file into a hidden field.

--- bridge/m1demo/static/js/solum-appdefine.js
+++ bridge/m1demo/static/js/solum-appdefine.js
@@ -19,7 +19,7 @@
         // General global variables
         var canvas, stage, queue, fps, glow_size, physics_max, plan_dom;
         var glow_start_color, glow_end_color, physics_on, orig_move_obj;
-        var mouse_status, last_mouse_obj;
+        var mouse_status, last_mouse_obj, planfile_html;
 
         // ***** Configuration *****
         // Screen positioning and distance configuration
@@ -48,6 +48,8 @@
         lb_x = 35;
         lb_y = 300;
         lb_conn_type = 0;
+        lb_camp_connect_type = "org.solum.reverseproxy:ConnectFrom";
+        lb_camp_char_type = "        characteristic_type: org.reverseproxy:Nginx\n";
 
         // *** Language Packs ***
         // Python
@@ -92,6 +94,10 @@
         trove_x = 760; // Trove icon x starting location
         trove_y = 300; // Trove icon y starting location
         trove_conn_type = 2;
+        trove_camp_connect_type = "org.solum.sql:ConnectTo";
+        trove_camp_char_type = "        characteristic_type: org.storage.db:RDBM\n" +
+            "        characteristic_type: org.storage.db:Replication\n" +
+            "        characteristic_type: org.iso.sql:SQL\n";
         // Memcache
         memcache_icon = "/static/images/memcache.png";
         memcache_name = "Memcached";
@@ -100,6 +106,8 @@
         memcache_x = 760; // Trove icon x starting location
         memcache_y = 400; // Trove icon y starting location
         memcache_conn_type = 2;
+        memcache_camp_connect_type = "org.solum.cache:ConnectTo";
+        memcache_camp_char_type = "        characteristic_type: org.solum.cache:Memcached\n";
         // Newrelic
         newrelic_icon = "/static/images/newRelic.png";
         newrelic_name = "NewRelic";
@@ -108,7 +116,8 @@
         newrelic_x = 760; // Trove icon x starting location
         newrelic_y = 200; // Trove icon y starting location
         newrelic_conn_type = 2;
-
+        newrelic_camp_connect_type = "org.solum.monitor:ConnectTo";
+        newrelic_camp_char_type = "        characteristic_type: org.solum.monitor:NewRelic\n";
 
 
         function solum_preinit() {
@@ -138,18 +147,24 @@
 
         function solum_init() {
             // Create the icon/draggers - Load Balancer first
-            solum_create_icon(lb_name, lb_x, lb_y, lb_x_scale, lb_y_scale, lb_conn_type);
+            solum_create_icon(lb_name, lb_x, lb_y, lb_x_scale, lb_y_scale, lb_conn_type,
+                lb_camp_connect_type, lb_camp_char_type);
             // Language Packs
-            solum_create_icon(python_name, python_x, python_y, python_x_scale, python_y_scale, python_conn_type);
-            solum_create_icon(java_name, java_x, java_y, java_x_scale, java_y_scale, java_conn_type);
-            solum_create_icon(ruby_name, ruby_x, ruby_y, ruby_x_scale, ruby_y_scale, ruby_conn_type);
-            solum_create_icon(nodejs_name, nodejs_x, nodejs_y, nodejs_x_scale, nodejs_y_scale, nodejs_conn_type);
+            solum_create_icon(python_name, python_x, python_y, python_x_scale, python_y_scale, python_conn_type,
+                "N/A", "N/A");
+            solum_create_icon(java_name, java_x, java_y, java_x_scale, java_y_scale, java_conn_type,
+                "N/A", "N/A");
+            solum_create_icon(ruby_name, ruby_x, ruby_y, ruby_x_scale, ruby_y_scale, ruby_conn_type,
+                "N/A", "N/A");
+            solum_create_icon(nodejs_name, nodejs_x, nodejs_y, nodejs_x_scale, nodejs_y_scale, nodejs_conn_type,
+                "N/A", "N/A");
             // Services
-            solum_create_icon(trove_name, trove_x, trove_y, trove_x_scale, trove_y_scale, trove_conn_type);
+            solum_create_icon(trove_name, trove_x, trove_y, trove_x_scale, trove_y_scale, trove_conn_type,
+                trove_camp_connect_type, trove_camp_char_type);
             solum_create_icon(memcache_name, memcache_x, memcache_y, memcache_x_scale,
-                memcache_y_scale, memcache_conn_type);
+                memcache_y_scale, memcache_conn_type, memcache_camp_connect_type, memcache_camp_char_type);
             solum_create_icon(newrelic_name, newrelic_x, newrelic_y, newrelic_x_scale,
-                newrelic_y_scale, newrelic_conn_type);
+                newrelic_y_scale, newrelic_conn_type, newrelic_camp_connect_type, newrelic_camp_char_type);
 
             var rect = new createjs.Shape();
             rect.graphics.beginFill("#F7DFF9").drawRect(0, 0, 70, 600);
@@ -193,20 +208,9 @@
             solum_create_text("Services:", "bold 17px Arial", 722, 120);
             solum_create_text("Network:", "bold 16px Arial", 2, 220);
 
-            // Made the Create Plan button
-            /*
-            html_button = document.getElementById("plan_button");
-            plan_dom = new createjs.DOMElement(html_button);
-            //plan_dom.x = 100;
-            plan_dom.y = 100;
-            plan_dom.htmlElement.onclick = function() {
-// TODO - connect this to the demo later
-                var plan_text = solum_create_plan();
-                if (plan_text.length < 5)
-                    plan_text = "[Empty plan file: No components are connected.]";
-                alert(plan_text);
-            }
-            */
+            // Plan text box
+            planfile_html = document.getElementById("planfile");
+            planfile_html.value = "camp_version: CAMP 1.1\nartifacts:\nservices:\n";
 
             // Create the main stage ticker to keep everything updated
             createjs.Ticker.setFPS(fps);
@@ -216,38 +220,88 @@
 
 
         function solum_create_plan() {
-// TODO remove hard coded crud
-            var plan_text = "name: ex1\n";
-            plan_text += "description: Nodejs express.\n";
-            plan_text += "artifacts:\n";
-            plan_text += "- name: nodeus\n";
-            plan_text += "  artifact_type: application.heroku\n";
-            plan_text += "   content:\n"
-            plan_text += "     href: https://github.com/paulczar/example-nodejs-express.git\n";
-            plan_text += "    language_pack: auto\n";
-            
-            plan_text += "\n\n----- Demo Connectivity Data -----\n{\n";
+// TODO: See if we can make this real later; solum doesn't understand the complete plan below yet
+//            return "name: ghost\ndescription: ghost blogging platform\nartifacts:\n" +
+//                "    name: ghost\n    artifact_type: application.heroku\n    content:\n" +
+//                "        href: https://github.com/paulczar/solum-example-app-ghost.git\n" +
+//                "    language_pack: auto\n"
+
+// TODO: add SQL initialization script; example:
+//   artifact_type: org.sql:SqlScript
+//    content:
+//        href: https://github.com/stackforge/solum.git
+//        script:solum/objects/sqlalchemy/migration/alembic_migrations/versions/498adc6185ae_create_initial_db_schema.py
+//    requirements:
+//        requirement_type: org.solum.sql:RunOn
+//        fulfillment: id:db_1
+
+            var plan_header = "camp_version: CAMP 1.1\n";
+            var artifacts_header = "artifacts:\n";
+            // Fill in ARTIFACT_URL with git repo url, NUM_INSTANCES with the number of
+            // apps to run.
+            var artifact_template = "    artifact_type: application.heroku\n" +
+                "    content:\n        href: ARTIFACT_URL\n    instances: NUM_INSTANCES\n" +
+                "    requirements:\n        requirement_type: org.solum:BuildUsing\n" +
+                "        fulfillment: id:langpack-auto\n\n";
+            // Fill in CONNECT_TYPE with "To" or "From", FULFILL_ID with the real id like "db_1"
+            // Also need to figure out the ??? sections
+            var artifact_connect_template = "        requirement_type: CONNECT_TYPE\n" +
+                "        fulfillment: id:FULFILL_ID\n\n";
+            var services_header = "services:";
+            // Fill in SERVICE_ID
+            var service_template = "\n    id: SERVICE_ID\n    characteristics:\n";
+            // Fill in CHAR_TYPE
+            var service_characteristic_template = "        characteristic_type: CHAR_TYPE\n";
+
+            // Hard coded example/test following...
+            var temp_str = "";
+            var out_str = plan_header + artifacts_header;
+
+            // DRY this up later
+            // Iterate artifacts aka language packs aka applications first
             for (var i = 0; i < stage.getNumChildren(); i++) {
                 obj = stage.getChildAt(i);
                 if (obj.hasOwnProperty("connection_list")) {
                     if (obj.connection_list.length > 0) {
-                        plan_text += "\t\"" + obj.icon_name + "_" + obj.id + "\": [";
-                        prev_entry = false;
-                        for (var j = 0; j < obj.connection_list.length; j++) {
-                            if (prev_entry == false) {
-                                prev_entry = true;
-                            } else {
-                                plan_text += ", ";
+                        if (obj.conn_type == 1) { // Artifact/Language Pack/Application/etc
+                            temp_str = artifact_template;
+// TODO: need git url
+                            temp_str = temp_str.replace("ARTIFACT_URL", "???artifact_url???");
+                            temp_str = temp_str.replace("NUM_INSTANCES", "1");
+                            out_str += temp_str;
+
+                            for (var j = 0; j < obj.connection_list.length; j++) {
+                                temp_str = artifact_connect_template;
+                                temp_str = temp_str.replace("CONNECT_TYPE", obj.connection_list[j][0].camp_connect_type);
+                                temp_str = temp_str.replace("FULFILL_ID", obj.connection_list[j][0].icon_name + "_" +
+                                    obj.connection_list[j][0].id);
+                                out_str += temp_str;
                             }
-                            plan_text += "\"" + obj.connection_list[j][0].icon_name +
-                                "_" + obj.connection_list[j][0].id + "\"";
                         }
-                        plan_text += "],\n";
+
                     }
                 }
             }
-            plan_text += "}";
-            return plan_text;
+
+            // Now iterate network and services
+            out_str += services_header;
+            for (var i = 0; i < stage.getNumChildren(); i++) {
+                obj = stage.getChildAt(i);
+                if (obj.hasOwnProperty("connection_list")) {
+                    if (obj.connection_list.length > 0) {
+                        if (obj.conn_type != 1) {
+                            temp_str = service_template;
+                            temp_str = temp_str.replace("SERVICE_ID",
+                                obj.icon_name + "_" + obj.id);
+                            out_str += temp_str;
+                            out_str += obj.camp_char_type;
+                        }
+
+                    }
+                }
+            }
+
+            return out_str;
         }
 
 
@@ -270,7 +324,8 @@
         }
 
 
-        function solum_create_icon(icon_name, x_pos, y_pos, x_scale, y_scale, conn_type) {
+        function solum_create_icon(icon_name, x_pos, y_pos, x_scale, y_scale, conn_type,
+            camp_connect_type, camp_char_type) {
             var bmp = new createjs.Bitmap(queue.getResult(icon_name));
             bmp.scaleX = x_scale;
             bmp.scaleY = y_scale;
@@ -290,6 +345,8 @@
             dragger.has_moved = false;
             dragger.connection_list = new Array();
             dragger.orig_conn_list = new Array();
+            dragger.camp_connect_type = camp_connect_type;
+            dragger.camp_char_type = camp_char_type;
 
             dragger.addChild(bmp);
             console.log("New container " + icon_name + "(" + dragger.id + ")");
@@ -341,6 +398,11 @@
                         
                         mouse_status = 0; // reset mouse status
                         last_mouse_obj = null;
+
+                        // Update planfile text after every connection/disconnection
+                        plan_text = solum_create_plan();
+                        planfile_html.value = plan_text;
+
                         return;
                     } else {
                         last_mouse_obj = evnt.currentTarget;
